7dcbbf18c58f724d57b5a1c204a0f4ad19c52d7f
Lipin Nambiar <lipinnambiar@gmail.com>
2021-05-13 14:40:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typingTest';
+  randomText = lorem.sentences();
+  userInput = '';
+
+  onInputChange(value: string) {
+    this.userInput = value;
+  }
+
+  compare(randomTextLetter: string, userEnterdText: string) {
+    if (!userEnterdText) {
+      return 'pending';
+    }
+
+    return randomTextLetter === userEnterdText ? 'correct' : 'incorrect';
+  }
 }
